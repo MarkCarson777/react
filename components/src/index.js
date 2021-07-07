7f55b93eff17e4414e456f787a72f2d83195a4ee
Mark Carson <markcarson121@gmail.com>
2021-07-07 15:42:25 +0100

    added comment and avatar props

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Mark" timeAgo="Today at 4:00PM"/>
-      <CommentDetail author="Andrew" timeAgo="Today at 6:00PM"/>
-      <CommentDetail author="Annie" timeAgo="Today at 8:00PM"/>
+      <CommentDetail author="Mark" timeAgo="Today at 4:00PM" comment="Great!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Andrew" timeAgo="Today at 6:00PM" comment="Awesome!" avatar={faker.image.avatar()} />
+      <CommentDetail author="Annie" timeAgo="Today at 8:00PM" comment="Brilliant!" avatar={faker.image.avatar()} />
     </div>
   );
 };
