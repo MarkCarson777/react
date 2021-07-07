37d9f93b59511f1cb434e80ff17b1d8703a546ac
Mark Carson <markcarson121@gmail.com>
2021-07-07 14:58:36 +0100

    duplicated post JSX

--- components/src/index.js
+++ components/src/index.js
@@ -21,6 +21,37 @@ const App = () => {
           </div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Mark
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">
+            Insert post here
+          </div>
+        </div>
+      </div> <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Mark
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">
+            Insert post here
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
