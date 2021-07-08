a9571698c7ac297d68fc21923a493d7a523374d9
Mark Carson <markcarson121@gmail.com>
2021-07-08 18:32:34 +0100

    implemented season display component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // functional based component
 // const App = () => {
@@ -42,7 +43,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
