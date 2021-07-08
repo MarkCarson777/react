5d19b2a48ed61deddb3fee5cad3a422ccde277c3
Mark Carson <markcarson121@gmail.com>
2021-07-08 18:52:54 +0100

    added icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -11,10 +11,13 @@ const getSeason = (lat, month) =>{
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const action = season === 'winter' ? 'Brr it\'s chilly' : 'Sun\'s out, gun\'s out';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{action}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
