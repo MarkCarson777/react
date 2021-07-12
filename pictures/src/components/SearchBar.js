78b2548b509e48a07e5d34d263ac1dab7ace595f
Mark Carson <markcarson121@gmail.com>
2021-07-12 14:42:13 +0100

    added event handler for user text input

--- pictures/src/components/SearchBar.js
+++ pictures/src/components/SearchBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
+          <b>Image Search</b>
           <div className="field">
-            <input type ="text" />
+          {/* onInputChange is a function with no parentheses as we don't want it to be run every time we render */}
+          {/* onChange is a JS property and cannot be called by any other name */}
+            <input type ="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
