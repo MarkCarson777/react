b052eb7a48eb6a9cd901d13abfbd4794b166cd1a
Mark Carson <markcarson121@gmail.com>
2021-07-12 14:45:52 +0100

    added pseudocode

--- pictures/src/components/SearchBar.js
+++ pictures/src/components/SearchBar.js
@@ -14,6 +14,8 @@ class SearchBar extends React.Component {
           {/* onInputChange is a function with no parentheses as we don't want it to be run every time we render */}
           {/* onChange is a JS property and cannot be called by any other name */}
             <input type ="text" onChange={this.onInputChange} />
+            {/* alternate version */}
+          {/* <input type="text" onChange={(event) => console.log(event.target.value)} />; */}
           </div>
         </form>
       </div>
