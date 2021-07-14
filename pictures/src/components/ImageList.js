d0627737ed1e1e66cb26c77b44287fb9039e35cc
Mark Carson <markcarson121@gmail.com>
2021-07-14 13:39:09 +0100

    added alt property to img element

--- pictures/src/components/ImageList.js
+++ pictures/src/components/ImageList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ImageList = (props) => {
   console.log(props.images);
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} />
+    return <img alt={image.description} key={image.id} src={image.urls.regular} />
   });
 
   return <div>{images}</div>;
