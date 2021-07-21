00a281260e1071c6f14b7e781400f1fa3485c8b0
Mark Carson <markcarson121@gmail.com>
2021-07-21 18:21:57 +0100

    added semantic ui to VideoList

--- youtube-clone/src/components/VideoItem.js
+++ youtube-clone/src/components/VideoItem.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      {video.snippet.title}
-      <img src={video.snippet.thumbnails.medium.url} />
+    <div className="item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
     </div>
   );
 };
