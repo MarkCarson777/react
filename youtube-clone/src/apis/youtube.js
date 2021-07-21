7944e71af6305616c63edfc4b90e49dc7bc54d93
Mark Carson <markcarson121@gmail.com>
2021-07-21 17:10:01 +0100

    installed axios

--- youtube-clone/src/apis/youtube.js
+++ youtube-clone/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAx8tEo7fwIPxn2EIHYSUFZoYa_bgjHof4';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
