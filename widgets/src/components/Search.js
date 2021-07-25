7aaba5857447a3de645798f17f487779f030cfa1
Mark Carson <markcarson121@gmail.com>
2021-07-25 20:28:39 +0100

    added wikipedia get request

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -4,12 +4,24 @@ import axios from 'axios';
 const Search = () => {
   const [term, setTerm] = useState('');
   
-  console.log('I run with every render');
 
   // useEffect example
+  // using async code in functional component
   useEffect(() => {
-    console.log('I only run once');
-  }, []);
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        }
+      });
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
