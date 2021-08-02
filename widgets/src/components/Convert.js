3bda858fd960e94382b755ad6b706f680cc8ca45
Mark Carson <markcarson121@gmail.com>
2021-08-02 16:58:34 +0100

    created post request for google languages

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
 useEffect(() => {
-  console.log('New language or text');
+  // first object is left empty as we don't want to send any information in the body in the post request
+  // we create a third argument/second object for query string parameters
+  axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+    params: {
+      q: text,
+      target: language.value,
+      key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+    }
+  })
 }, [language, text]);
 
   return <div />
