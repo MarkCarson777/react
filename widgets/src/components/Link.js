a10a6947a55f80453ae3ac8f77cab4ac163d0d9a
Mark Carson <markcarson121@gmail.com>
2021-08-02 18:43:49 +0100

    added pushState to chnage URL

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const Link = ({className, href, children}) => {
+const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+    window.history.pushState({}, '', href);
   };
 
   return (
     <a 
       onClick={onClick} 
-      classnName={className} 
+      className={className} 
       href={href}
     >
       {children}
     </a>
-  )
+  );
 };
 
 export default Link;
