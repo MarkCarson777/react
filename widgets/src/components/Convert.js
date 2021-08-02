70de7b0b94521c672be84ec5232a4963c390c53a
Mark Carson <markcarson121@gmail.com>
2021-08-02 17:09:37 +0100

    added useEffect for translation

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-useEffect(() => {
-  // first object is left empty as we don't want to send any information in the body in the post request
-  // we create a third argument/second object for query string parameters
-  axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-    params: {
-      q: text,
-      target: language.value,
-      key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-    }
-  })
+  const [translated, setTranslated] = useState('');
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      // first object is left empty as we don't want to send any information in the body in the post request
+      // we create a third argument/second object for query string parameters
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      });
+
+      setTranslated(data.data.translations[0].translatedText)
+    };
+
+  doTranslation();
 }, [language, text]);
 
-  return <div />
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  )
 };
 
 export default Convert;
