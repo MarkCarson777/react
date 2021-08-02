7ae0638c8a0c2022fbee81941392a1c284f16d69
Mark Carson <markcarson121@gmail.com>
2021-08-02 18:00:10 +0100

    imported Route into App.js

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -34,29 +35,29 @@ const options = [
   }
 ];
 
-const showAccordion = () => {
-  if (window.location.pathname === '/') {
-    return <Accordion items={items} />;
-  }
-}
+// const showAccordion = () => {
+//   if (window.location.pathname === '/') {
+//     return <Accordion items={items} />;
+//   }
+// }
+// 
+// const showList = () => {
+//   if (window.location.pathname === '/list') {
+//     return <Search />;
+//   }
+// }
 
-const showList = () => {
-  if (window.location.pathname === '/list') {
-    return <Search />;
-  }
-}
-
-const showDropdown = () => {
-  if (window.location.pathname === '/dropdown') {
-    return <Dropdown />;
-  }
-}
-
-const showTranslate = () => {
-  if (window.location.pathname === '/translate') {
-    return <Translate />;
-  }
-}
+// const showDropdown = () => {
+//   if (window.location.pathname === '/dropdown') {
+//     return <Dropdown />;
+//   }
+// }
+// 
+// const showTranslate = () => {
+//   if (window.location.pathname === '/translate') {
+//     return <Translate />;
+//   }
+// }
 
 // const showComponent = (route, component) => {
 //   return window.location.pathname === route ? component : null;
@@ -68,10 +69,12 @@ export default () => {
 
   return (
     <div>
-      {showAccordion()}
-      {showList()}
-      {showDropdown()}
-      {showTranslate()}
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
       {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button> */}
       {/* {showDropdown ? */}
       {/*   <Dropdown  */}
--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
-const Route = () => {
-
+const Route = ({ path, children }) => {
+  return window.location.pathname === path ? children : null;
 };
 
 export default Route;
