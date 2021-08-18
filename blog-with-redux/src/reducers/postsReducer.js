43a6ef830c8656672a98081b9076db7650c94ca0
Mark Carson <markcarson121@gmail.com>
2021-08-18 16:38:57 +0100

    created switch statement in postsReducer

--- blog-with-redux/src/reducers/postsReducer.js
+++ blog-with-redux/src/reducers/postsReducer.js
@@ -1,3 +1,15 @@
-export default () => {
-  return 123;
+export default (state = [], action) => {
+//   if (action.type === 'FETCH_POSTS') {
+//     return action.payload;
+//   }
+// 
+//   return state;
+
+  // switch is a valid JS function that does the same as the above
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 };
