08e7acb91b05fe0ba0fbf6ded6407c6e9a61ff19
Mark Carson <markcarson121@gmail.com>
2021-08-19 17:40:06 +0100

    added refactored version  of fetchPostsAndUsers using lodash chain

--- blog-with-redux/src/actions/index.js
+++ blog-with-redux/src/actions/index.js
@@ -7,6 +7,13 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 
   const userIds = _.uniq(_.map(getState().posts, 'userId'));
   userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // REFACTORED VERSION WITH LODASH CHAIN
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
 };
 
 export const fetchPosts = () => async dispatch => {
