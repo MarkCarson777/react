cfa274d592a3a117073054d4f2c8c60c1f41f8a6
Mark Carson <markcarson121@gmail.com>
2021-08-26 15:09:33 +0100

    imported redux-form into StreamCreate

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+// allows as to rename the in-built reducer function as something more descriptive
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
