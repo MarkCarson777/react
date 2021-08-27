fb66d63651eeb94ab246259e355d30a0eca65f67
Mark Carson <markcarson121@gmail.com>
2021-08-27 14:15:34 +0100

    added semantic ui edit and delete buttons

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -10,7 +10,16 @@ class StreamList extends React.Component {
   // helper function
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
-      return <div>Edit/Delete</div>
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">
+            Edit
+          </button>
+          <button className="ui button negative red">
+            Delete
+          </button>
+        </div>
+      );
     };
   }
   
@@ -19,12 +28,12 @@ class StreamList extends React.Component {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
