e2ddcdd88d9861f766de835b1644d4bc0962fc22
Mark Carson <markcarson121@gmail.com>
2021-08-31 09:37:24 +0100

    installed redux and thunk

--- umi-tech/src/components/App.js
+++ umi-tech/src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import UserDetails from './UserDetails';
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <UserDetails />  
+    </div>
+  );
 };
 
 export default App;
